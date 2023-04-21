890a448d692f128dfb99923583ad6b55d32663cf
trannamtrung1st <trannamtrung1st@gmail.com>
2023-04-21 10:28:57 +0700

    Add project files.

---
+++ RateLimiting/Controllers/ResourcesController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RateLimiting.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ResourcesController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<ResourcesController> _logger;
+
+        public ResourcesController(ILogger<ResourcesController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            var rng = new Random();
+            return Enumerable
+                .Range(1, 5)
+                .Select(index => Summaries[rng.Next(Summaries.Length)])
+                .ToArray();
+        }
+    }
+}
